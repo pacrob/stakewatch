d44528fb1a376c9adfca8ef40f305fa2553d1ed0
pacrob <paul@pacrob.com>
2022-01-24 16:51:40 -0700

    add datetime and card background colors

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 from web3 import Web3
 from flask import Flask, render_template, json
+from datetime import datetime
 
 app = Flask(__name__)
-
-with open("config.json", "r") as config:
-    data = json.load(config)
 
 def get_chain_info(url: str):
     w3 = Web3(Web3.HTTPProvider(url))
@@ -18,8 +16,13 @@ def get_chain_info(url: str):
 
     return connected, chain_id, latest_block
 
+with open("config.json", "r") as config:
+    data = json.load(config)
+
 @app.route("/")
 def index():
+
+    warning, danger = data["block_thresholds"]["warning"], data["block_thresholds"]["danger"]
 
     truth = data["source_of_truth"]
     truth["connected"], truth["chain_id"], truth["latest_block"] = get_chain_info(truth["url"])
@@ -32,11 +35,24 @@ def index():
 
     for staker in stakers:
         staker["connected"], staker["chain_id"], staker["latest_block"] = get_chain_info(staker["url"])
+        
+        block_diff = truth["latest_block"] - staker["latest_block"]
+        if block_diff > danger:
+            staker["background"] = "bg-danger"
+        elif block_diff > warning:
+            staker["background"] = "bg-warning"
+        else:
+            staker["background"] = "bg-success"
 
-    print(stakers)
+
+    print(f"{stakers=}")
+
+    now = datetime.now()
+    print(f"{now=}")
 
     return render_template(
         "index.html",
         source_of_truth = truth,
-        stakers = stakers
+        stakers = stakers,
+        time = now,
     )
